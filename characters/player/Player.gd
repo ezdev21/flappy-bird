cc150a12283480db5a96c06a93d0edc18d0a8cf0
gech11 <getachewfikadu77@gmail.com>
2021-12-20 22:29:44 +0300

    player script reimplemnted

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -6,17 +6,26 @@ var started=false
 onready var animator=$AnimationPlayer
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed('flap'):
-	   if !started:
-		  start()
-		  flap()
+	 if Input.is_action_just_pressed('flap'):
+		  if !started:
+			  start() 
+			  flap()
+	 if rotation_degrees===MAX_ROTATION_DEGREES:
+		angular_veloccity=0
+		rotation_degrees=MAX_ROTATION_DEGREES
+	 if linear_velocity.y>0:
+		if rotation_degrees<=90:
+			angular_velocity=3
+		else:
+			angular_velocity=0	
+			
 func start():
 	if started: return
 	started=true
 	gravity_scale=5.0
 	animator.play('flap')
 	
-	
 func flap():
 	linear_velocity.y=FLAP_FORCE
+	angular_velocity=-8.0
 		
